aef1cf679eb99c708dcbb4099edde9ffc5a30f8c
nadavlie <n.liberman95@gmail.com>
2022-12-04 12:11:06 +0200

    implementing selector logic

--- src/main.ts
+++ src/main.ts
@@ -1,2 +1,81 @@
-import './style.scss'
-// keep the line above and write your code below.
+import "./style.scss";
+
+const exchangeRateEndpoint = "https://currency-ror1.vercel.app/api/currency";
+const datesCurrenciesArrayEndpoint =
+  "https://currency-ror1.vercel.app/api/dates-table";
+
+const currencyCodes = {
+  USD: "01",
+  EUR: "27",
+  GBP: "02",
+  CAD: "06",
+  AUD: "18",
+  GPY: "31",
+};
+
+/**
+ * Use these strings in the form select (drop-down) -
+ * you can copy-paste into the HTML
+ * and/or use them programmatically
+ */
+
+const currencyNames = [
+  "USD (United States Dollar)",
+  "EUR (Euro)",
+  "GBP (Great Britain Pound)",
+  "CAD (Canadian Dollar)",
+  "AUD (Australian Dollar)",
+  "GPY (Japanese Yen)",
+];
+
+window.addEventListener("DOMContentLoaded", (event) => {
+  attachListeners();
+  return populateTable();
+});
+
+function attachListeners() {
+  const currencyConverterForm = document.getElementById(
+    "currency-converter-form"
+  );
+  currencyConverterForm.onsubmit = getExchangeRateFromForm;
+}
+
+/**
+ * Complete the function below to get the exchange rate from the API
+ */
+
+async function getExchangeRateFromApi(dateCode: string, currencyCode: string) {}
+
+/**
+ * Complete the function below to get the data from the form,
+ * send it to the API, present the result, and show/hide the spinner.
+ * The two event methods prevent the form submission from reloading the page.
+ */
+
+async function getExchangeRateFromForm(event) {
+  event.preventDefault();
+  event.stopPropagation();
+}
+
+/**
+ * Complete the function below to get the array of dates and currencies,
+ * create table rows, get the exchange rates from the API,
+ * present the results, and show/hide the spinner
+ */
+
+async function populateTable() {
+  return null;
+}
+console.log("hello");
+
+// <option value="Vanilla"></option>
+const currency_list = document.querySelector("#currency-list");
+const currency_choice = document.querySelector("#currency_choice");
+
+currency_choice?.addEventListener("click", function () {
+  for (const item of currencyNames) {
+    const html = `<option value="${item}"></option>`;
+    console.log(item);
+    currency_list.insertAdjacentHTML("afterbegin", html);
+  }
+});
